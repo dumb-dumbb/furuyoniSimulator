dff9d3ad0bc2a3d4c6a26a38926bee39df985957
cireisen <kdh_214@naver.com>
2022-12-26 00:19:12 +0900

    기본동작 추가

--- Assets/Script/EventQueue.cs
+++ Assets/Script/EventQueue.cs
@@ -42,6 +42,18 @@ public class EventQueue : MonoBehaviour
                 case "forward":
                     Forward(time.me, time.game);
                     break;
+                case "backward":
+                    Backward(time.me, time.game);
+                    break;
+                case "recover":
+                    Recover(time.me, time.game);
+                    break;
+                case "concentrate":
+                    Concentrate(time.me, time.game);
+                    break;
+                case "breakaway":
+                    Breakaway(time.me, time.game);
+                    break;
                 case "na_01_yurina_o_n_5_s5_1":
                     AddFocus(time.me, 1);
                     break;
@@ -98,26 +110,132 @@ public class EventQueue : MonoBehaviour
 
     }
 
-    void Forward(Player player, SingleGame game)
+    /// <summary>
+    /// 기본동작/전진
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="game"></param>
+    private void Forward(Player player, SingleGame game)
     {
         int n = CountAvailableSakura(game.distance, 1);
-        if(player.aura + n > player.maxAura)
+
+        if(player.GetAura() + n > player.GetMaxAura())
         {
             return;
         }
 
         int distance = game.distance;
-        int aura = player.aura;
+        int aura = player.GetAura();
 
         moveSakura(ref distance, ref aura, 1);
 
         game.distance = distance;
-        player.aura = aura;
+        player.SetAura(aura);
 
-        Debug.Log(player.aura);
+        Debug.Log(player.GetAura());
         Debug.Log(game.distance);
     }
 
+    /// <summary>
+    /// 기본동작/후퇴
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="game"></param>
+    private void Backward(Player player, SingleGame game)
+    {
+        int aura = player.GetAura();
+        int distance = game.distance;
+        int n = CountAvailableSakura(aura, 1);
+
+        if (aura <= 0 || distance >= 10)
+        {
+            return;
+        }
+
+        moveSakura(ref aura, ref distance, 1);
+
+        game.distance = distance;
+        player.SetAura(aura);
+
+        Debug.Log(player.GetAura());
+        Debug.Log(game.distance);
+    }
+
+    /// <summary>
+    /// 기본동작/휘감기
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="game"></param>
+    private void Recover(Player player, SingleGame game)
+    {
+        int aura = player.GetAura();
+        int dust = game.dust;
+
+        int n = CountAvailableSakura(game.dust, 1);
+
+        if (player.GetAura() >= player.GetMaxAura() || dust <= 0)
+        {
+            return;
+        }
+
+        moveSakura(ref dust, ref aura, 1);
+
+        game.dust = dust;
+        player.SetAura(aura);
+
+        Debug.Log(player.GetAura());
+        Debug.Log(game.dust);
+    }
+
+    /// <summary>
+    /// 기본동작/품기
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="game"></param>
+    private void Concentrate(Player player, SingleGame game)
+    {
+        int aura = player.GetAura();
+        int flare = player.GetFlare();
+
+        int n = CountAvailableSakura(game.dust, 1);
+
+        if (player.GetAura() <= 0)
+        {
+            return;
+        }
+
+        moveSakura(ref aura, ref flare, 1);
+
+        player.SetFlare(flare);
+        player.SetAura(aura);
+
+        Debug.Log(player.GetAura());
+        Debug.Log(player.GetAura());
+    }
+
+    /// <summary>
+    /// 기본동작/이탈
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="game"></param>
+    private void Breakaway(Player player, SingleGame game)
+    {
+        int dust = game.dust;
+        int distance = game.distance;
+
+        if (dust <= 0 || distance >= 10)
+        {
+            return;
+        }
+
+        moveSakura(ref dust, ref distance, 1);
+
+        game.dust = dust;
+        game.distance = distance;
+
+        Debug.Log(game.dust);
+        Debug.Log(game.distance);
+    }
 
     /// <summary>
     ///  집중력 추가
@@ -125,10 +243,10 @@ public class EventQueue : MonoBehaviour
     void AddFocus(Player player, int num)
     {
         // TODO: 위축 상태일 때 num을 1 깎는 거 추가
-        if(player.focus + num > 2)
-            player.focus = 2;
+        if(player.GetFocus() + num > 2)
+            player.SetFocus(2);
         else
-            player.focus += num;
+            player.SetFocus(player.GetFocus() + num);
     }
 
     void DrawCard(Player player, SingleGame game)
