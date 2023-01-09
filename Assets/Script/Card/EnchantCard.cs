8b445108b81b60c204da030e7967cc8388a83771
cireisen <kdh_214@naver.com>
2023-01-09 21:22:41 +0900

    카드수

---
+++ Assets/Script/Card/EnchantCard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Script
+{
+    public class EnchantCard : Card
+    {
+        private int stockedSakura;
+
+        private int stockedSeed;
+
+        public EnchantCard(string a, string b) : base(a, b)
+        {
+            
+        }
+    }
+}
